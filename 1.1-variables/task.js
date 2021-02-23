109fe630f8f3c654d212cd621a9525afcbb77c62
Vera Vasilyeva <lilililo@mail.ru>
2021-02-23 20:26:29 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -15,7 +15,7 @@ function averageMark(a, g, p) {
 
 function sayHello(userName) {
 	let myName = userName;
-	let message = "Привет, мир! Меня зовут " + myName + ".";
+	let message = "Привет, мир! Меня зовут " + myName;
 	return message;
 }
 
