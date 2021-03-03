55db0a24624ae3e759e1b6b304eb686005d1da07
Vera Vasilyeva <lilililo@mail.ru>
2021-03-03 21:26:14 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,22 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let mortgageBody = amount - contribution; 
+    let months = (date.getMonth() + date.getFullYear() * 12) - ((new Date().getMonth()) + (new Date().getFullYear()) * 12);
+    let monthlyPercent = 1 / 12 * percent / 100; 
+    let monthlyPayment = mortgageBody * (monthlyPercent + monthlyPercent/((Math.pow((1+monthlyPercent), months)-1)));   
+    let totalAmount = months * monthlyPayment; 
+    totalAmount = +totalAmount.toFixed(2);    
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if (name === "" || name === null || name === undefined) {        
+        greeting = `Привет, мир! Меня зовут Аноним.` 
+        console.log(greeting);
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}.` 
+        console.log(greeting);
+    }    
+    return greeting;
 }
