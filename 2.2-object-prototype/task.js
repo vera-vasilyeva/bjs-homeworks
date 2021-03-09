367193479c70397e694dc6d46fc48babde7e71ef
Vera Vasilyeva <lilililo@mail.ru>
2021-03-09 19:33:03 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,30 @@
-//String.prototype.isPalindrome - для задачи №1
-
+//Задача №1
+String.prototype.isPalindrome = function isPalindrome() {
+    let string = this.toLowerCase().replace(/\s/g, "").split("");
+    console.log(string);
+    console.log(string.reverse());
+    return string.join("") === (string.reverse().join(""));
+}
+//Задача №2
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < marks.length; i++) {
+            sum += marks[i];
+        }
+        let average = sum / marks.length;
+        let roundedAverage = Math.round(average);
+        return roundedAverage;
+    }
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Number(new Date());
+    let birthdayDate = new Date(birthday);
+    let birthDay = birthdayDate.getTime();
+    let diff = now - birthDay;
+    let age = diff / 31536000000;
+    return (age > 18);
 }
